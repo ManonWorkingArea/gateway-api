1b249f804bd17ba9c84cccfe6c93ef1775adc971
Manon Sanoi <info@manonsanoi.com>
2023-12-14 06:29:50 +0700

    Add Request Log

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,44 @@ function setupRoutes(app) {
       next();
     }
   });
+
+  // Add this middleware to log request details
+  app.use('/api', (req, res, next) => {
+    console.log('Request URL:', req.url);
+    console.log('Request Base URL:', req.baseUrl);
+    console.log('Request Method:', req.method);
+    console.log('Request Parameters:', req.params);
+
+    // Iterate over query parameters and log their names and values
+    for (const key in req.query) {
+      if (Object.hasOwnProperty.call(req.query, key)) {
+        const value = req.query[key];
+        console.log(`Query Parameter ${key}:`, value);
+      }
+    }
+
+    // Get client IP address
+    const clientIP = req.ip;
+    console.log('Client IP Address:', clientIP);
+
+    // Get client details from user-agent header
+    const userAgent = req.headers['user-agent'];
+    console.log('User-Agent:', userAgent);
+
+    // You can use a library like `useragent` to parse user-agent string
+    const useragent = require('useragent');
+    const agent = useragent.parse(userAgent);
+
+    console.log('Operating System:', agent.os.toString());
+    console.log('Browser:', agent.toAgent());
+
+    if (req.method === 'POST') {
+      console.log('Request Body Data:', req.body);
+    }
+
+    next();
+  });
+
   const connections = {};
   const sourceFile = sourceMap.mongodb;
   const sourceRoutes = require(sourceFile);
