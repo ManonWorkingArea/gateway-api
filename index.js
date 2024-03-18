c8c2a73120d4227290a63c7adb9acfbbf87dba35
Manon Sanoi <info@manonsanoi.com>
2024-03-19 03:48:34 +0700

    ฑฟะำ ศรทระ

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const socketio      = require('socket.io'); // Import socket.io module
 dotenv.config();
 
 const app = express();
+
+const rateLimit = require('express-rate-limit');
+
+// Create the rate limit rule
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limit to all requests
+app.use(apiLimiter);
 app.set('trust proxy', true);
 app.use(bodyParser.json());
 app.use(cors());
