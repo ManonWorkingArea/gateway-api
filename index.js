a908f0e5937ee2c1e755abe1f1199cf0cabb686b
Manon Sanoi <info@manonsanoi.com>
2024-10-04 19:10:15 +0700

    cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const apiLimiter = rateLimit({
 //app.use(apiLimiter);
 app.set('trust proxy', false);
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
   console.log(`Incoming request: ${req.method} ${req.url}`);
   next();
